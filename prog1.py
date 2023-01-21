9ffd197f1f77dff70626c25ab01a675767763558
Vikash <viksh.yadav3@gmail.com>
2023-01-21 03:35:03 -0500

    i'm adding new file

---
+++ prog1.py
@@ -0,0 +1,9 @@
+class Employee:
+    def __init__(self):
+        print("This is My First Project in GitHub !")
+
+    def disp(self):
+        print("Now, i start my first project !")
+
+emp=Employee()
+emp.disp()
